da7763ad8188057d9dc6c18f65dd5ef765c45ace
arsenic-art <piyush782005@gmail.com>
2026-01-08 20:08:40 +0530

    fixed routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -5,6 +5,6 @@ const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/verify-email", verifyEmail);
+router.post("/verify-email", verifyEmail);
 
 export default router;
--- server/src/routes/fundraiser.routes.ts
+++ server/src/routes/fundraiser.routes.ts
@@ -13,10 +13,10 @@ const router = Router();
 
 router.post("/", protect, createFundraiser);
 router.get("/", getAllFundraisers);
+router.get("/me", protect, getMyFundraisers);
 router.get("/:id", getFundraiserById);
 
 router.patch("/:id", protect, updateFundraiser);
 router.patch("/:id/close", protect, closeFundraiser);
-router.get("/me", protect, getMyFundraisers);
 
 export default router;
